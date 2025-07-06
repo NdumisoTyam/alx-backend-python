db5502150630c90da9489cbe68954693c60132e1
NdumisoTyam <Ndumisotyam1@gmail.com>
2025-07-06 22:43:40 +0200

    Create 1-batch_processing.py

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import mysql.connector
+
+def stream_users_in_batches(batch_size):
+    """Generator that fetches rows from user_data table in batches of size batch_size."""
+    conn = mysql.connector.connect(
+        host='localhost',
+        user='root',      # change if needed
+        password='',      # change if needed
+        database='ALX_prodev'
+    )
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT user_id, name, email, age FROM user_data;")
+
+    while True:
+        batch = cursor.fetchmany(batch_size)
+        if not batch:
+            break
+        yield batch
+
+    cursor.close()
+    conn.close()
+
+def batch_processing(batch_size):
+    """Processes batches to yield only users with age > 25."""
+    for batch in stream_users_in_batches(batch_size):
+        for user in batch:
+            if user['age'] > 25:
+                yield user
