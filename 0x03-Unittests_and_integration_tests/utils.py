95541cf957d12455e1470221d8ac185ac887cfde
NdumisoTyam <Ndumisotyam1@gmail.com>
2025-08-08 18:46:50 +0200

    Update utils.py

--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -1,13 +1,25 @@
 # utils.py
 
 import requests
+from functools import wraps
 
 def get_json(url):
     response = requests.get(url)
     return response.json()
 
 def access_nested_map(nested_map, path):
-    """Access a nested dictionary using a list of keys."""
     for key in path:
         nested_map = nested_map[key]
     return nested_map
+
+def memoize(fn):
+    """Simple memoization decorator."""
+    attr_name = "_{}".format(fn.__name__)
+
+    @wraps(fn)
+    def memoized(self):
+        if not hasattr(self, attr_name):
+            setattr(self, attr_name, fn(self))
+        return getattr(self, attr_name)
+
+    return memoized
