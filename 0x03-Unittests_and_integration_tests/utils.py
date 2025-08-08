ac06f89b33c11e684c851be7cc946fc63fbb4937
NdumisoTyam <Ndumisotyam1@gmail.com>
2025-08-08 18:43:24 +0200

    Update utils.py

--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -5,3 +5,9 @@ import requests
 def get_json(url):
     response = requests.get(url)
     return response.json()
+
+def access_nested_map(nested_map, path):
+    """Access a nested dictionary using a list of keys."""
+    for key in path:
+        nested_map = nested_map[key]
+    return nested_map
