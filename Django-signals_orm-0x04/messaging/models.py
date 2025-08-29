5c2f3fe3ee2d77c4412056af429ff225999f2fa4
NdumisoTyam <Ndumisotyam1@gmail.com>
2025-08-29 17:20:28 +0200

    Update models.py

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -6,9 +6,10 @@ class Message(models.Model):
     receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+    edited = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"From {self.sender} to {self.receiver} at {self.timestamp}"
+        return f"{self.sender} → {self.receiver} | Edited: {self.edited}"
 
 class Notification(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
