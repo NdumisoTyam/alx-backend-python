6835f46519be96eabe329002facdb61a35d5d035
NdumisoTyam <Ndumisotyam1@gmail.com>
2025-09-06 18:56:09 +0200

    Delete Django-signals_orm-0x04 directory

--- Django-signals_orm-0x04/messaging/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Message, Notification
-from .models import Message, MessageHistory
-
-admin.site.register(Message)
-admin.site.register(Notification)
-admin.site.register(MessageHistory)
--- Django-signals_orm-0x04/messaging/utils.py
+++
@@ -1,5 +0,0 @@
-def get_thread(message):
-    thread = [message]
-    for reply in message.replies.select_related('sender', 'receiver').all():
-        thread.extend(get_thread(reply))
-    return thread
